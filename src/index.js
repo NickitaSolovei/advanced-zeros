4767c44cfee691821121d69dbc44986dc46e7cb7
NickitaSolovei <43146250+NickitaSolovei@users.noreply.github.com>
2018-09-17 18:29:34 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
+  var maxSimpleNum = base;
+  var ins = 1;
+
+  function fff(simpleNum) {
+    var maxSimpleNum22 = simpleNum;
+    for (var i = 2; i <= simpleNum/2 ; i++){
+      var ostatok = simpleNum % i;
+      if (ostatok === 0){
+
+        maxSimpleNum22 = i;
+      }
+    }
+    if (simpleNum !== maxSimpleNum22){
+      maxSimpleNum = maxSimpleNum22;
+      ins++;
+      fff(maxSimpleNum);
+    }
+  }
+  fff(maxSimpleNum);
   // your implementation
+  var answer1 = 0;
+
+  function divide5(n){
+    var newN = n / maxSimpleNum;
+    if (newN >= 1){
+      newN = Math.floor(newN);
+      answer1 += newN;
+      divide5(newN);
+    }
+  }
+  divide5(number);
+
+  return answer1;
 }
