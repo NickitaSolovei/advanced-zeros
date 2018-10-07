34753256d71c4335a403f2e5fe67fbb380ceb377
NickitaSolovei <43146250+NickitaSolovei@users.noreply.github.com>
2018-10-07 23:12:01 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,50 +1,52 @@
 module.exports = function getZerosCount(number, base) {
-  var maxSimpleNum = base;
-  var ins = 1;
-  var ins2 = 1;
-
-  function fff(simpleNum) {
-    var maxSimpleNum22 = simpleNum;
-    for (var i = 2; i <= simpleNum/2 ; i++){
-      var ostatok = simpleNum % i;
-      if (ostatok === 0){
-
-        maxSimpleNum22 = i;
+  let maxSimpleNum = base;
+  let ins = 1;
+  
+  function getMaxSimpleNum(simpleNum) {
+    let maxSimpleNumFun = simpleNum;
+    for (let i = 2; i <= simpleNum/2 ; i++){
+      let balance = simpleNum % i;
+      if (balance === 0){
+        maxSimpleNumFun = i;
       }
     }
-    if (simpleNum !== maxSimpleNum22){
-      maxSimpleNum = maxSimpleNum22;
+    if (simpleNum !== maxSimpleNumFun){
+      maxSimpleNum = maxSimpleNumFun;
       ins++;
-      fff(maxSimpleNum);
+      getMaxSimpleNum(maxSimpleNum);
     }
   }
-  fff(maxSimpleNum);
-  // your implementation
-  var answer1 = 0;
-
-  function divide5(n){
-    var newN = n / maxSimpleNum;
+  getMaxSimpleNum(maxSimpleNum);
+  let answer = 0;
+  
+  function divideBase(n){
+    let newN = n / maxSimpleNum;
     if (newN >= 1){
       newN = Math.floor(newN);
-      answer1 += newN;
-      divide5(newN);
+      answer += newN;
+      divideBase(newN);
     }
   }
-  divide5(number);
-
-  var k1 = 1;
+  divideBase(number);
+  
+  let k1 = 1;
   function SearchK1(x){
     if (x % (maxSimpleNum * maxSimpleNum) === 0) {
       k1++;
-      var x2 = x / maxSimpleNum;
+      let x2 = x / maxSimpleNum;
       SearchK1(x2);
     };
   }
-
   SearchK1(base);
+  answer = Math.floor(answer / k1);
 
-    answer1 = Math.floor(answer1 / k1);
-  
-
-  return answer1;
+  let answer2 = answer;
+  k1 = 1;
+  maxSimpleNum = 2;
+  answer = 0;
+  divideBase(number);
+  SearchK1(base);
+  answer = Math.floor(answer / k1);
+  if (answer > answer2) return answer2
+  else return answer; 
 }
